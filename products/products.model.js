6937f7a4df5b6cc70adfde424da4743d56e1be29
rizamoyi <samwelmoyi@gmail.com>
2022-07-10 13:12:03 +0300

    Mutations

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
     id: 'redshoe',
     description: 'Red Shoe',
     price: 42.12,
+    reviews: [],
   },
   {
     id: 'bluejeans',
     description: 'Fancy Blue Jeans',
     price: 55.55,
+    reviews: [],
   },
 ];
 
@@ -25,8 +27,36 @@ function getProductById(id) {
   return products.find((product) => product.id === id);
 }
 
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    description,
+    price,
+    reviews: [],
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductById(id);
+
+  if (matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+
+    matchedProduct.reviews.push(newProductReview);
+
+    return newProductReview;
+  }
+}
 module.exports = {
   getAllProducts,
   getProductsByPrice,
   getProductById,
+  addNewProduct,
+  addNewProductReview,
 };
